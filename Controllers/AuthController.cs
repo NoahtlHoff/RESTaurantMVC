128a641513c1c4e0ef1817fa731ab2fec788247f
Noah <noah.hoff@outlook.com>
2025-09-21 17:00:32 +0200

    rm codex

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
