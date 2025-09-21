0c499ac940917e0bb13928293318492ce75d46d2
Noah Hoff <noah.hoff@outlook.com>
2025-09-21 16:57:18 +0200

    Resolve auth merge conflicts

--- Models/AuthTokenResponse.cs
+++ Models/AuthTokenResponse.cs
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-﻿namespace RESTaurantMVC.Models
+using System;
+
+namespace RESTaurantMVC.Models
 {
     public class AuthTokenResponse
     {
         public string Token { get; set; } = string.Empty;
-        //public DateTimeOffset? ExpiresAt { get; set; }
+        public DateTimeOffset? ExpiresAt { get; set; }
     }
-=======
-using System;
-
-namespace RESTaurantMVC.Models;
-
-public class AuthTokenResponse
-{
-    public string Token { get; set; } = string.Empty;
-    public DateTimeOffset? ExpiresAt { get; set; }
->>>>>>> cb1d0dd99a364873478e5922fa6853247b2cc6df
 }
